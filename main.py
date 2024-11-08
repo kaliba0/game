b365a617b3dcfdc7eb3bca23fb0a62f1ef9a17e7
NSI <aterzn@gmail.com>
2024-11-08 09:49:48 +0100

    pendunsi

---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+
+RED_BOLD = '\033[1;91m'
+BOLD = '\033[1m'
+RESET = '\033[0m'
+BLUE_BOLD = '\033[1;34m'
+YELLOW_BOLD = '\033[1;32m'
+
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def show_banner():
+
+    banner = RED_BOLD + """
+╔───────────────────────────────────────────────╗
+│                                               │
+│  ██████╗ ███████╗███╗   ██╗██████╗ ██╗   ██╗  │
+│  ██╔══██╗██╔════╝████╗  ██║██╔══██╗██║   ██║  │
+│  ██████╔╝█████╗  ██╔██╗ ██║██║  ██║██║   ██║  │
+│  ██╔═══╝ ██╔══╝  ██║╚██╗██║██║  ██║██║   ██║  │
+│  ██║     ███████╗██║ ╚████║██████╔╝╚██████╔╝  │
+│  ╚═╝     ╚══════╝╚═╝  ╚═══╝╚═════╝  ╚═════╝   │
+│                                               │
+╚───────────────────────────────────────────────╝
+    """ + RESET
+
+    
+    
+    infos = f'{BOLD} CREATED ON 11/24 BY {BLUE_BOLD} ANTOINE {RED_BOLD} (NOT KILLIAN) {RESET}'
+
+    print(banner)
+    print(infos)
+
+
+def show_menu():
+    menu = BOLD + """
+
+    =============== OPTIONS ===============
+
+             1.  Start a new game
+    """ + RESET
+    print(menu)
+
+
+
+
+def update_prompt(state):
+    prompt=''
+    if state == "menu":
+        prompt = RED_BOLD + 'pendu/menu>' + RESET
+        return prompt
+    elif state == "ingame":
+        prompt = YELLOW_BOLD + 'pendu/game>' + RESET
+        return prompt
+
+
+def main():
+    prompt = update_prompt("menu")
+    clear_screen()
+    show_banner()
+    show_menu()
+
+    while True:
+        choice = input(prompt)
+        if (choice == '1' or choice == '1 '):
+            update_prompt("ingame")
+        else:
+            print(RED_BOLD + "INVALID CHOICE. PLEASE TRY AGAIN" + RESET)
+
+        
+if __name__ == "__main__":
+    main()
